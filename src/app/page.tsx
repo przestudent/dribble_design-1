c34801af40087933eb65ca7b570623bba0c3aea6
Przemek Pałac <przemekpalac@student.agh.edu.pl>
2023-08-03 12:00:23 +0200

    Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,20 @@ import Link from 'next/link';
 
 function LandingPage() {
   return (
-    <div className='w-[100vw] h-[100vh] grid grid-cols-1 grid-rows-1 items-center'>
-      <img src='/bg.png' className='col-start-1 col-end-2' />
-      <div className='flex flex-col text-center col-start-1 col-end-2 '>
+    <div className='w-[100vw] h-[100vh] items-center'>
+      <div className='flex flex-col text-center  p-4'>
         <Link className='text-7xl border-slate-50 border-2' href={'/furniture'}>
-          Check it out
+          Check it out <span className='animate-pulse'>▶</span>
         </Link>
         <a
-          className='text-3xl text-pink-500'
+          className='text-3xl underline underline-offset-2 text-pink-500'
           href='https://dribbble.com/shots/21753130-Int-rieur-Interior-Design-Agency-Landing-Page-Website'
         >
           Original Dribble
         </a>
+      </div>
+      <div className='flex justify-center'>
+        <img src='/bg.png' className='' />
       </div>
     </div>
   );
